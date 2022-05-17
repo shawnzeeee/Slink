fa5670b682ff306bd0d5ce4f3146e3eae0b6d90b
shawnzeeee <51132367+shawnzeeee@users.noreply.github.com>
2022-05-17 16:38:52 -0700

    Add files via upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "slinkefef4a56": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
